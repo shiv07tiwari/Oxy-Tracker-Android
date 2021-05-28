77551400ffa79b03f6e121030e3580250d270632
Komal Bhadani <komal.b@fampay.in>
2021-05-29 00:52:47 +0530

    use another library

--- app/src/main/java/com/example/oxygencylindertracker/qrcode/QRScannerActivity.kt
+++ app/src/main/java/com/example/oxygencylindertracker/qrcode/QRScannerActivity.kt
@@ -1,53 +1,34 @@
 package com.example.oxygencylindertracker.qrcode
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
+import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.oxygencylindertracker.R
-import com.google.zxing.integration.android.IntentIntegrator
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+import com.google.zxing.Result
 
+class QRScannerActivity: Activity(), ZXingScannerView.ResultHandler {
+    lateinit var mScannerView: ZXingScannerView
 
-class QRScannerActivity : AppCompatActivity() {
-    lateinit var integrator: IntentIntegrator
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qrscanner)
-        integrator = IntentIntegrator(this)
-        integrator.setOrientationLocked(false)
-        integrator.initiateScan()
+    override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        mScannerView = ZXingScannerView(this) // Programmatically initialize the scanner view
+        mScannerView.setAutoFocus(true)
+        setContentView(mScannerView) // Set the scanner view as the content view
     }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String?>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == 101) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show()
-//                integrator.initiateScan()
-//            } else {
-//                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
-//            }
-//        }
-//    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-            }
-        }
+    override fun onResume() {
+        super.onResume()
+        mScannerView.setResultHandler(this) // Register ourselves as a handler for scan results.
+        mScannerView.startCamera() // Start camera on resume
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mScannerView.stopCamera() // Stop camera on pause
+    }
+
+    override fun handleResult(rawResult: Result) {
+        Toast.makeText(this, rawResult.getText(), Toast.LENGTH_SHORT).show()
+        mScannerView.resumeCameraPreview(this)
     }
 }
